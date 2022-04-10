2ccfbeff09ea936818c8d6c3b22752db73380f6d
Ubong Eduok <ubongeduok33@gmail.com>
2022-04-10 09:53:04 +0100

    Set up sanity, and done with left sidebar

---
+++ client/components/Feeds.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Feeds = () => {
+    return (
+        <h2>Hello feed</h2>
+    )
+}
+
+export default Feeds;
---
+++ client/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import  Feeds  from "../components/Feeds"
+import SideBar from '../components/SideBar'
+
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`
+}
+
+const Home: NextPage = () => {
+  return (
+     <div className={style.wrapper}>
+       <div className={style.content}>
+            <SideBar />
+            <Feeds />
+            <h6>Widgets</h6>
+       </div>
+     </div>
+  )
+}
+
+export default Home
